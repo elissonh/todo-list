460f7dd94bf16b54c933ecaa4496ef872ef87460
Elisson <elisson3004@gmail.com>
2026-06-10 22:49:40 -0300

    feat: update api mapping url

--- backend/src/main/java/com/devlson/to_do_list/controllers/TaskController.java
+++ backend/src/main/java/com/devlson/to_do_list/controllers/TaskController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class TaskController {
@@ -24,20 +25,20 @@ public class TaskController {
         this.taskMapper = taskMapper;
     }
 
-    @PostMapping(path = "/tasks")
+    @PostMapping(path = "/api/tasks")
     public TaskDto createTask(@RequestBody TaskDto task) {
         TaskEntity taskEntity = taskMapper.mapFrom(task);
         TaskEntity result = taskService.createTask(taskEntity);
         return taskMapper.mapTo(result);
     }
 
-    @GetMapping(path = "/tasks")
+    @GetMapping(path = "/api/tasks")
     public Page<TaskDto> listTasks(Pageable pageable) {
         Page<TaskEntity> tasks = taskService.findAll(pageable);
         return tasks.map(taskMapper::mapTo);
     }
 
-    @GetMapping(path = "/tasks/{id}")
+    @GetMapping(path = "/api/tasks/{id}")
     public ResponseEntity<TaskDto> getTask(@PathVariable("id") Long id) {
         Optional<TaskEntity> task =  taskService.findOne(id);
         return task.map(taskEntity -> {
@@ -46,7 +47,7 @@ public class TaskController {
         }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @PutMapping(path = "/tasks/{id}")
+    @PutMapping(path = "/api/tasks/{id}")
     public ResponseEntity<TaskDto> updateTask(@PathVariable("id") Long id, @RequestBody TaskDto taskDto) {
         if(!taskService.exists(id)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -60,16 +61,17 @@ public class TaskController {
                 HttpStatus.OK);
     }
 
-    @PatchMapping(path = "/tasks/{id}")
-    public ResponseEntity<TaskDto> patchTask(@PathVariable("id") Long id, @RequestBody TaskDto taskDto) {
-        if(!taskService.exists(id)) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @PatchMapping(path = "/api/tasks/{id}")
+    public ResponseEntity<TaskDto> patchTask(@PathVariable Long id, @RequestBody TaskDto taskDto) {
+        // if(!taskService.exists(id)) {
+        //     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        // }
+        TaskEntity existingTask = taskService.findOne(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (taskDto.getDescription() == null) {
+            taskDto.setDescription(existingTask.getDescription());
         }
-
-        taskDto.setId(id);
-
-        TaskEntity existingTask = taskService.findOne(id).orElseThrow(() -> new RuntimeException("Task not found"));
-        taskDto.setDescription(existingTask.getDescription());
 
         taskDto.setId(id);
         TaskEntity taskEntity = taskMapper.mapFrom(taskDto);
@@ -78,7 +80,7 @@ public class TaskController {
         return new ResponseEntity<>(taskMapper.mapTo(updatedTask), HttpStatus.OK);
     }
 
-    @DeleteMapping(path = "tasks/{id}")
+    @DeleteMapping(path = "/api/tasks/{id}")
     public ResponseEntity<TaskDto> deleteTask(@PathVariable Long id) {
         taskService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
